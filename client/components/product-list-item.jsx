09d7789fa8f6da7a7355c20341ef6f4c465252aa
Vicky Huynhcao <vicky89@gmail.com>
2019-11-19 17:57:08 +0000

    fixed changes requested: Removed constructor in product-list-item.jsx

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 
-class ProductListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
-
-  render() {
-    const imgSize = {
-      maxHeight: '200px',
-      objectFit: 'contain'
-    };
-    return (
-      <div className="col-12 col-md-4 d-flex align-items-stretch my-3">
-        <div className="card">
-          <img className="card-img-top" style={imgSize} src={this.props.image}/>
-          <div className="card-body d-flex flex-column justify-content-around">
-            <h2 className="card-title">{this.props.name}</h2>
-            <h4 className="card-subtitle mb-2 text-muted">{'$' + (this.props.price / 100).toFixed(2)}</h4>
-            <p className="card-text">{this.props.info}</p>
-          </div>
+function ProductListItem(props) {
+  const imgSize = {
+    maxHeight: '200px',
+    objectFit: 'contain'
+  };
+  return (
+    <div className="col-12 col-md-4 d-flex align-items-stretch my-3">
+      <div className="card">
+        <img className="card-img-top" style={imgSize} src={props.image}/>
+        <div className="card-body d-flex flex-column justify-content-around">
+          <h2 className="card-title">{props.name}</h2>
+          <h4 className="card-subtitle mb-2 text-muted">{'$' + (props.price / 100).toFixed(2)}</h4>
+          <p className="card-text">{props.info}</p>
         </div>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 
 export default ProductListItem;
