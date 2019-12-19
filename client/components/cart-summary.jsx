1947b259c9f71fdc4d78ad768e35e949adaf9f64
Vicky Huynhcao <vicky89@gmail.com>
2019-12-19 23:39:58 +0000

    Made a condition to show a message if the cart is empty if not empty, then show the items

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -6,6 +6,21 @@ function CartSummary(props) {
 
   let totalPrice = 0;
   cartItems.map(product => (totalPrice = totalPrice + product.price));
+
+  let display = null;
+  if (cartItems.length === 0) {
+    display = (
+      <h4 className="text-danger">No items in cart</h4>
+    );
+  } else {
+    display = (
+      <>
+        {cartItems.map(item => {
+          return <CartSummaryItems key={item.productId} cartInfo={item} />;
+        })}
+      </>
+    );
+  }
   return (
     <div>
       <div
@@ -15,9 +30,7 @@ function CartSummary(props) {
         {'< Back to Catalog'}
       </div>
       <h1>My Cart</h1>
-      {cartItems.map(item => {
-        return <CartSummaryItems key={item.productId} cartInfo={item} />;
-      })}
+      {display}
       <h2>Total Price {'$' + (totalPrice / 100).toFixed(2)}</h2>
     </div>
   );
